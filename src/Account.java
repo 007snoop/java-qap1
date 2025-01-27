c056d4a9d21aff803fcbcb98d89e3916565dfa3d
Colin Yetman <colinguitarist@gmail.com>
2025-01-27 08:45:18 -0330

    dynamically update ID for uniqueness

--- src/Account.java
+++ src/Account.java
@@ -1,44 +1,47 @@
 public class Account {
     // attributes
-    private String id;
+    private int id;
     private String firstName;
     private String lastName;
     private int balance = 0;
 
     // constructors
 
-/*     public Account(String id, String firstName, String lastName) {
-        this.id = "";
-        this.firstName = "";
-        this.lastName = "";
+    public Account() {
+        this.id = 0;
+        this.firstName = "no name";
+        this.lastName = "no name";
     }
- */
 
-    public Account(String id, String firstName, String lastName, int balance) {
-        this.id = id;
+
+    public Account(int id, String firstName, String lastName, int balance) {
+       
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.id = ++id;
     }
     // behavior
-    public String getID() {
+
+    public int getID() {
         return id;
     }
-    
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public int setID() {
+        return ++this.id ;
     }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getFullName() {
         return firstName + " " + lastName;
     }
+    public int getBalance() {
+        return balance;
+    }
 
+    public int addCredit(int amount) {
+        this.balance += amount;
+        return balance; 
+    }
     //toString Method
-
+    @Override
     public String toString()
     {
         return ("ID: " + this.id +  " " + "Name: " + this.firstName + " " + this.lastName + " "+ "Balance: " + this.balance);
--- src/TestAccount.java
+++ src/TestAccount.java
@@ -1,21 +1,20 @@
 public class TestAccount {
     public static void main(String[] args) {
-     /*    Account A1 = new Account("1", "colin", "yetman");
-        System.out.println(A1); */
-        Account Acc1 = new Account("2", "Annie", "Malik", 5000);
-    
-        Account Acc2 = new Account("3", "Noah", "Devine", 4000);
+        Account A1 = new Account();
+        Account Acc2 = new Account(2, "Noah", "Devine", 4000);
+        Account Acc1 = new Account(1, "Annie", "Malik", 5000);
+        Account tesAccount = new Account();
+        System.out.println(A1);
+        Acc1.setID();
+        Acc2.setID();
         System.out.println(Acc1 + " First Print.");
         // set methods
-        Acc1.setFirstName("Colin");
-        Acc1.setLastName("Yetman");
-        System.out.println(Acc1 + " Second Print.");
+        System.out.println(Acc2 + " Second Print.");
         // get method for fullname
-        String FullName = Acc1.getFullName();
-        System.out.println(FullName);
-        // get method for id
-        String ID = Acc2.getID();
-        System.out.println(ID);
+        tesAccount.setID();
+        Acc1.addCredit(1000);
+        System.out.println(Acc1);;
+        System.out.println(tesAccount);
         
     }
 }
