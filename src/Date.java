a89439041bfcba32713db30ba9f4c16183c92608
Colin Yetman <colinguitarist@gmail.com>
2025-01-28 21:29:14 -0330

    problem #2 update

--- src/Date.java
+++ src/Date.java
@@ -5,14 +5,47 @@ public class Date {
     private int year;
 
     // constructors
+    public Date() {
+        this.day = 01;
+        this.month = 01;
+        this.year = 1990;
+    }
+
     public Date(int day, int month, int year) {
-        this.day = 1;
-        this.month = 1;
-        this.year = 1900;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    // getter
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    // setters
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     @override
-    public String toString() {
+    public String toString() { 
         return String.format("%02d/%02d/%04d", day, month, year);
     }
 }
--- src/TestDate.java
+++ src/TestDate.java
@@ -1,6 +1,10 @@
 public class TestDate {
     public static void main(String[] args) {
         Date date = new Date();
+
+        date.setDate(4, 2, 19);
         System.out.println(date);
+
+        int day = date.getDay();
     }
 }
