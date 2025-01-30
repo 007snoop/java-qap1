d9bdace086569086cee1f3713f90378a88c6a1b5
Colin Yetman <colinguitarist@gmail.com>
2025-01-30 08:44:38 -0330

    problem #3 solved. Added IO for testing.

--- src/TestTime.java
+++ src/TestTime.java
@@ -3,5 +3,14 @@ public class TestTime {
         Time t1 = new Time(21,10,15);
         Time t2 = new Time(10, 20, 25);
 
+
+        System.out.println(t1 + " First print");
+        System.out.println(t2 + " First print");
+
+        t1.counterUpSecond();
+        t2.counterDownSecond();
+
+        System.out.println(t1 + " Second print");
+        System.out.println(t2 + " Second print");
     }
 }
